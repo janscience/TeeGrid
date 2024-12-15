6afc0a25d2face520af679f58d97c06dc4f39e2d
Jan Benda <jan.benda@uni-tuebingen.de>
2024-12-15 22:56:45 +0100

    [loggerconf] new SpinBox widget with units

--- utils/loggerconf.py
+++ utils/loggerconf.py
@@ -24,13 +24,13 @@ try:
 except ImportError:
     from PyQt5.QtCore import pyqtSignal as Signal
 from PyQt5.QtCore import Qt, QObject, QTimer, QDateTime, QLocale
-from PyQt5.QtGui import QKeySequence, QFont, QPalette, QColor
+from PyQt5.QtGui import QKeySequence, QFont, QPalette, QColor, QValidator
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtWidgets import QStackedWidget, QLabel, QScrollArea
 from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout, QSpacerItem
 from PyQt5.QtWidgets import QWidget, QFrame, QPushButton, QSizePolicy
 from PyQt5.QtWidgets import QAction, QShortcut
-from PyQt5.QtWidgets import QCheckBox, QLineEdit, QComboBox, QSpinBox, QDoubleSpinBox
+from PyQt5.QtWidgets import QCheckBox, QLineEdit, QComboBox, QSpinBox, QAbstractSpinBox
 
 
 __version__ = '1.0'
@@ -78,6 +78,108 @@ def parse_number(s):
     u = s[n:].strip()
     nd = n - ip if n >= ip else 0
     return v, u, nd
+
+
+unit_prefixes = {'Deka': 1e1, 'deka': 1e1, 'Hekto': 1e2, 'hekto': 1e2,
+                 'kilo': 1e3, 'Kilo': 1e3, 'Mega': 1e6, 'mega': 1e6,
+                 'Giga': 1e9, 'giga': 1e9, 'Tera': 1e12, 'tera': 1e12, 
+                 'Peta': 1e15, 'peta': 1e15, 'Exa': 1e18, 'exa': 1e18, 
+                 'Dezi': 1e-1, 'dezi': 1e-1, 'Zenti': 1e-2, 'centi': 1e-2,
+                 'Milli': 1e-3, 'milli': 1e-3, 'Micro': 1e-6, 'micro': 1e-6, 
+                 'Nano': 1e-9, 'nano': 1e-9, 'Piko': 1e-12, 'piko': 1e-12, 
+                 'Femto': 1e-15, 'femto': 1e-15, 'Atto': 1e-18, 'atto': 1e-18, 
+                 'da': 1e1, 'h': 1e2, 'K': 1e3, 'k': 1e3, 'M': 1e6,
+                 'G': 1e9, 'T': 1e12, 'P': 1e15, 'E': 1e18, 
+                 'd': 1e-1, 'c': 1e-2, 'mu': 1e-6, 'u': 1e-6, 'm': 1e-3,
+                 'n': 1e-9, 'p': 1e-12, 'f': 1e-15, 'a': 1e-18}
+""" SI prefixes for units with corresponding factors. """
+
+
+def change_unit(val, old_unit, new_unit):
+    """Scale numerical value to a new unit.
+
+    From https://github.com/bendalab/audioio/blob/master/src/audioio/audiometadata.py
+    which is adapted from https://github.com/relacs/relacs/blob/1facade622a80e9f51dbf8e6f8171ac74c27f100/options/src/parameter.cc#L1647-L1703
+
+    Parameters
+    ----------
+    val: float
+        Value given in `old_unit`.
+    old_unit: str
+        Unit of `val`.
+    new_unit: str
+        Requested unit of return value.
+
+    Returns
+    -------
+    new_val: float
+        The input value `val` scaled to `new_unit`.
+
+    Examples
+    --------
+
+    ```
+    >>> from audioio import change_unit
+    >>> change_unit(5, 'mm', 'cm')
+    0.5
+
+    >>> change_unit(5, '', 'cm')
+    5.0
+
+    >>> change_unit(5, 'mm', '')
+    5.0
+
+    >>> change_unit(5, 'cm', 'mm')
+    50.0
+
+    >>> change_unit(4, 'kg', 'g')
+    4000.0
+
+    >>> change_unit(12, '%', '')
+    0.12
+
+    >>> change_unit(1.24, '', '%')
+    124.0
+
+    >>> change_unit(2.5, 'min', 's')
+    150.0
+
+    >>> change_unit(3600, 's', 'h')
+    1.0
+
+    ```
+
+    """
+    # missing unit?
+    if not old_unit and not new_unit:
+        return val
+    if not old_unit and new_unit != '%':
+        return val
+    if not new_unit and old_unit != '%':
+        return val
+
+    # special units that directly translate into factors:
+    unit_factors = {'%': 0.01, 'hour': 60.0*60.0, 'h': 60.0*60.0, 'min': 60.0}
+
+    # parse old unit:
+    f1 = 1.0
+    if old_unit in unit_factors:
+        f1 = unit_factors[old_unit]
+    else:
+        for k in unit_prefixes:
+            if len(old_unit) > len(k) and old_unit[:len(k)] == k:
+                f1 = unit_prefixes[k];
+  
+    # parse new unit:
+    f2 = 1.0
+    if new_unit in unit_factors:
+        f2 = unit_factors[new_unit]
+    else:
+        for k in unit_prefixes:
+            if len(new_unit) > len(k) and new_unit[:len(k)] == k:
+                f2 = unit_prefixes[k];
+  
+    return val*f1/f2
 
 
 def get_teensy_model(vid, pid, serial_number):
@@ -919,7 +1021,109 @@ class YesNoQuestion(QWidget):
     def reject(self):
         self.yes = False
 
-        
+
+class SpinBox(QAbstractSpinBox):
+
+    textChanged = Signal(str)
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setAccelerated(True)
+        self.setAlignment(Qt.AlignLeft)
+        self.setButtonSymbols(QAbstractSpinBox.UpDownArrows)
+        self.setCorrectionMode(QAbstractSpinBox.CorrectToPreviousValue)
+        self.setFrame(True)
+        self.setKeyboardTracking(True)
+        self.setReadOnly(False)
+        self.setGroupSeparatorShown(False)
+        self.setWrapping(False)
+        self._minimum = None
+        self._maxmimum = None
+        self._decimals = 0
+        self._value = 0
+        self._unit = ''
+
+    def setDecimals(self, n):
+        self._decimals = n
+
+    def minimum(self):
+        return self._minimum
+
+    def maximum(self):
+        return self._maximum
+
+    def setMinimum(self, minv):
+        self._minimum = minv
+
+    def setMaximum(self, maxv):
+        self._maximum = maxv
+
+    def setStepType(self, stype):
+        pass
+
+    def stepEnabled(self):
+        steps = self.StepUpEnabled | self.StepDownEnabled
+        try:
+            if self._minimum is not None and self._value <= self._minimum:
+                steps &= ~self.StepDownEnabled
+            if self._maximum is not None and self._value >= self._maximum:
+                steps &= ~self.StepUpEnabled
+        except AttributeError:  # why does it not know self._minimum initially?
+            pass
+        return steps
+
+    def setValue(self, value):
+        self._value = value
+        locale = QLocale()
+        text = f'{value:.{self._decimals}f}'
+        text = text.replace('.', locale.decimalPoint())
+        text += self._unit
+        self.lineEdit().setText(text)
+
+    def setSuffix(self, suffix):
+        self._unit = suffix
+
+    def validate(self, text, pos):
+        locale = QLocale()
+        s = text.replace(locale.decimalPoint(), '.')
+        value, unit, ndec = parse_number(s)
+        if value is None:
+            return QValidator.State.Intermediate, text, pos
+        if self._minimum is not None and value <= self._minimum:
+            return QValidator.State.Intermediate, text, pos
+        if self._maximum is not None and value >= self._maximum:
+            return QValidator.State.Intermediate, text, pos
+        if ndec > self._decimals:
+            return QValidator.State.Intermediate, text, pos
+        self._value = float(value)
+        if not self._unit and unit:
+            text = text[:-len(unit)]
+        self.textChanged.emit(text)
+        #return QValidator.State.Intermediate
+        #return QValidator.State.Invalid
+        return QValidator.State.Acceptable, text, pos
+
+    def fixup(self, text):
+        locale = QLocale()
+        s = text.replace(locale.decimalPoint(), '.')
+        value, unit, ndec = parse_number(s)
+        if value is None:
+            value = self._minimum  # TODO should be previous value
+        if self._minimum is not None and value <= self._minimum:
+            value = self._minimum
+        if self._maximum is not None and value >= self._maximum:
+            value = self._maximum
+        if ndec > self._decimals:
+            ndec = self._decimals
+        text = f'{value:.{ndec}f}'.replace('.', locale.decimalPoint())
+        text += unit
+        self.setValue(float(value))
+        self.textChanged.emit(text)
+
+    def stepBy(self, steps):
+        self.setValue(self._value + steps)
+
+    
 class Parameter(Interactor, QObject, metaclass=InteractorQObject):
     
     def __init__(self, ids, name, value, num_value=None,
@@ -1032,7 +1236,7 @@ class Parameter(Interactor, QObject, metaclass=InteractorQObject):
             self.edit_widget.setValue(self.num_value)
             self.edit_widget.textChanged.connect(self.transmit_str)
         elif self.type_str in ['integer', 'float']:
-            self.edit_widget = QDoubleSpinBox(parent)
+            self.edit_widget = SpinBox(parent)
             self.edit_widget.setDecimals(self.ndec)
             if self.min_val is not None:
                 if self.out_unit:
@@ -1040,7 +1244,8 @@ class Parameter(Interactor, QObject, metaclass=InteractorQObject):
                 else:
                     minv = float(self.min_val)
                 self.edit_widget.setMinimum(minv)
-            if self.edit_widget.minimum() >= 0:
+            if self.edit_widget.minimum() is not None and \
+               self.edit_widget.minimum() >= 0:
                 self.edit_widget.setStepType(QSpinBox.AdaptiveDecimalStepType)
             if self.max_val is not None:
                 if self.out_unit:
@@ -1050,9 +1255,9 @@ class Parameter(Interactor, QObject, metaclass=InteractorQObject):
                 self.edit_widget.setMaximum(maxv)
             else:
                 self.edit_widget.setMaximum(1e9)
-            self.edit_widget.setValue(self.num_value)
             if self.out_unit:
                 self.edit_widget.setSuffix(self.out_unit)
+            self.edit_widget.setValue(self.num_value)
             self.edit_widget.textChanged.connect(self.transmit_str)
         elif self.type_str == 'string':
             self.edit_widget = QLineEdit(self.value, parent)
@@ -1104,7 +1309,11 @@ class Parameter(Interactor, QObject, metaclass=InteractorQObject):
             s = s.replace(locale.groupSeparator(), '')
             s = s.replace(locale.decimalPoint(), '.')
             svalue, sunit, _ = parse_number(s)
-            self.matches = abs(value - svalue) < 1e-6 and unit == sunit
+            if svalue is None and s == self.special_str:
+                svalue = self.special_val
+            if self.out_unit:
+                svalue = change_unit(svalue, sunit, unit)
+            self.matches = abs(value - svalue) < 1e-6
         elif self.type_str == 'string':
             self.matches = (self.edit_widget.text() == text)
         if self.matches:
