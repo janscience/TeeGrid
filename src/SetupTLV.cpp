2c1e156bc651e596c1917fcd8d48ba09d54636db
Jan Benda <jan.benda@uni-tuebingen.de>
2026-08-19 15:22:54 +0200

    [SetupTLV] fixed channel ordering

--- src/SetupTLV.cpp
+++ src/SetupTLV.cpp
@@ -53,10 +53,14 @@ void R5SetupTLVs(Input &aidata, const InputSettings &aisettings,
 		  tlvs[k]->TDMBus(), 'A' + tlvs[k]->TDMPin());
     R5SetupTLV(inputdata, *tlvs[k], k%2==1, aitdmsettings);
   }
-  if (ncontrols > 6)
+  if (inputdata.nchannels() > 24)
     static_cast<InputTDM&>(aidata).setRoll(8);
+  else
+    static_cast<InputTDM&>(aidata).setRoll(0);
   if (aitdmsettings.reverseInputs())
     static_cast<InputTDM&>(aidata).setReverse(4);
+  else
+    static_cast<InputTDM&>(aidata).setReverse(1);
   stream.println();
 }
 
