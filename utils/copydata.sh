922ccc0a05ae80dc39afaec81e539803181461b0
Jan Benda <jan.benda@uni-tuebingen.de>
2026-08-29 17:16:24 +0200

    [utils] added wavpack support to copydata

--- utils/copydata.sh
+++ utils/copydata.sh
@@ -2,6 +2,9 @@
 
 echo "Available SD cards and data:"
 for disk in /media/$USER/*; do
+    test "${PWD:0:${#disk}}" = "$disk" && continue
+    diskname=${disk#/media/$USER/}
+    test "${diskname:0:1}" = "d" && continue
     echo
     echo "$disk :"
     for data in $disk/*; do
@@ -14,14 +17,48 @@ echo
 echo -n "copy all data to $PWD? [Y/n] "
 read resp
 test -z "$resp" && resp='y'
-
 test "$resp" != "y" && exit 1
+
+mode="w"
+#echo
+#echo "Use"
+#echo "[r] rsync (no compression)"
+#echo "[w] wavpack (wav-file compression)"
+#echo "Select "
+#read mode
+#test "$mode" != "r" && test "$mode" != "g" && test "$mode" != "w" && exit 1
+
 echo
 
 start_time="$(date -u +%s)"
 for disk in /media/$USER/*; do
-    echo "copy $disk ..."
-    rsync -av $disk/* . &
+    test "${PWD:0:${#disk}}" = "$disk" && continue
+    diskname=${disk#/media/$USER/}
+    test "${diskname:0:1}" = "d" && continue
+    if test "$mode" = "r"; then
+	echo "copy $disk using rsync ..."
+    elif test "$mode" = "w"; then
+	echo "copy $disk using wavpack ..."
+    fi
+    {
+	for path in $disk/*; do
+	    if test -d "$path"; then
+		destpath=${path#$disk/}
+		site=${destpath%%-*}
+		grid=${destpath#$site-}
+		grid=${grid%%-*}
+		destpath="$site/$grid/$destpath"
+		mkdir -p $destpath
+		if test "$mode" = "r"; then
+		    rsync -av $path $destpath
+		elif test "$mode" = "w"; then
+		    wavpack -q -f --no-overwrite -t $path/*.wav -o $destpath
+		fi
+		chmod -R a-w $destpath
+		chmod -R a+r $destpath
+	    fi
+	done
+    } &
 done
 wait
 end_time="$(date -u +%s)"
@@ -30,8 +67,25 @@ elapsed="$(($end_time-$start_time))"
 echo
 echo "finished copying in ${elapsed}s!"
 
-# 4 SD cards:  0.37GB/s
-# 1 SD card:   0.18GB/s
+## TODO
+# do not copy 0byte files
+# naming: siteXX-gridYY-devID2-TIME
+# sort into directory structure siteX/gridY/timeZ/*.wav
 
+# 1 SD card slot to internal harddrive: rsync 86 MB/s
+# 1 SD card slot to internal harddrive: gzip 6 MB/s
+# 1 SD card slot to internal harddrive: wavpack 50 MB/s
 
+# 1 SD card USB to internal harddrive: rsync 154 MB/s  100%
+# 1 SD card USB to internal harddrive: gzip 12 MB/s
+# 1 SD card USB to internal harddrive: wavpack 52 MB/s  21%
 
+# 4 SD card USB to internal harddrive: rsync   328 MB/s  100%
+# 4 SD card USB to internal harddrive: wavpack 125 MB/s   20%
+
+# 4 SD card USB to USB harddrive: rsync      222 MB/s  100%
+# 4 SD card USB to USB harddrive: wavpack    106 MB/s   20%
+# 4 SD card USB to USB harddrive: wavpack -f 130 MB/s   23%  <--!!!
+
+# 4 x 265GB/2day = 8155s = 2.5hours !
+# 25logger * 133GB/day@48kHz * 12Tage = 40TB uncompressed = 10TB compressed
